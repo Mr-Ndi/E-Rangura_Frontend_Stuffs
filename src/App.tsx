9584dc7256b7b3d8577e729a6e6d315179b211ce
Mr-Ndi <onlythenotes@gmail.com>
2024-11-19 10:53:19 +0200

    Managing the process of calling components

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import HeroSection from './components/HeroSection/HeroSection'
+import GallerySection from './components/GallerySection/Gallerysection';
 
 function App() {
     return (
         <Router>
             <div className="App">
                 <Navbar />
-                <HeroSection />
+                <Routes>
+                    <Route path= "/" element={< HeroSection />} />
+                    <Route path="/gallery" element={<GallerySection />} />
+                </Routes>
                 <Footer />
             </div>
         </Router>
---
+++ src/components/GallerySection/Gallerysection.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GallerySection: React.FC = () => {
+    return (
+        <div className="gallery-section">
+            <h2>Gallery Page</h2>
+            {/* Add content specific to the gallery here */}
+            <p>Welcome to the gallery section. Add your images or content here.</p>
+        </div>
+    );
+};
+
+export default GallerySection;
