de920877fd01f9bef75ffdf76cec83687768f103
Mr-Ndi <onlythenotes@gmail.com>
2024-11-28 18:34:46 +0200

    Sub final contact page

--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './ContactSection.css'; 
+import './ContactSection.css';
 
 const Contact = () => {
   const [formData, setFormData] = useState({
@@ -9,7 +9,7 @@ const Contact = () => {
     message: ''
   });
 
-  const handleChange = (e) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { id, value } = e.target;
     setFormData((prevState) => ({
       ...prevState,
@@ -17,7 +17,7 @@ const Contact = () => {
     }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log('Form Data Submitted:', formData);
     // Add your form submission logic here
@@ -29,48 +29,60 @@ const Contact = () => {
         Contact <span>Us</span>
       </h1>
       <div className="container">
-        <div className="contact_box">
-          <div className="left">
-            <h2>We'd Love to Hear From You!</h2>
-            <p>Feel free to reach out with any questions or feedback.</p>
-          </div>
-          <div className="right">
+        <div className="left">
+          <h2>We'd Love to Hear From You!</h2>
+          <p>Feel free to reach out with any questions or feedback.</p>
+        </div>
+        <div className="right">
+          <div className="contact_box">
             <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                id="name"
-                className="field"
-                placeholder="Your Name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-              />
-              <input
-                type="email"
-                id="email"
-                className="field"
-                placeholder="Your Email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-              <input
-                type="tel"
-                id="phone"
-                className="field"
-                placeholder="Your Phone"
-                value={formData.phone}
-                onChange={handleChange}
-                required
-              />
-              <textarea
-                id="message"
-                className="field"
-                placeholder="Message"
-                value={formData.message}
-                onChange={handleChange}
-                required
-              ></textarea>
+              <div className="input">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  className="field"
+                  placeholder="Your Name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="input">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  className="field"
+                  placeholder="Your Email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="input">
+                <label htmlFor="phone">Number</label>
+                <input
+                  type="tel"
+                  id="phone"
+                  className="field"
+                  placeholder="Your Phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="input">
+                <label htmlFor="message">Message</label>
+                <textarea
+                  id="message"
+                  className="field"
+                  placeholder="Message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  required
+                ></textarea>
+              </div>
               <button type="submit" className="contact_btn">
                 Send
               </button>
