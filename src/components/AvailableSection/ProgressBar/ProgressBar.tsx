b233c1b9a287ba3c7b20eff6128ab416ebc4e238
Mr-Ndi <onlythenotes@gmail.com>
2024-12-14 18:50:09 +0200

    final progressbar transformation

--- src/components/AvailableSection/ProgressBar/ProgressBar.tsx
+++ src/components/AvailableSection/ProgressBar/ProgressBar.tsx
@@ -2,21 +2,24 @@ import React from "react";
 import './ProgressBar.css';
 
 interface ProgressBarProps {
-    progress: number; // Expecting a number between 0 and 100
+    progress: number;
 }
 
 const ProgressBar: React.FC<ProgressBarProps> = ({ progress }) => {
-    // Ensure progress is within the range of 0 to 100
+
     const clampedProgress = Math.min(100, Math.max(0, progress));
 
-    // Dynamic class based on progress
+
     const progressClass = clampedProgress === 100 ? 'progress-complete' : '';
 
     return (
-        <div className="progress-bar-container" role="progressbar" aria-valuenow={clampedProgress} aria-valuemin={0} aria-valuemax={100}>
-            <div 
-                className={`progress-bar ${progressClass}`} 
-                style={{ width: `${clampedProgress}%` }}>
+        <div className="byose">
+            <p>Fetching from our database ...</p>
+            <div className="progress-bar-container" role="progressbar" aria-valuenow={clampedProgress} aria-valuemin={0} aria-valuemax={100}>
+                <div 
+                    className={`progress-bar ${progressClass}`} 
+                    style={{ width: `${clampedProgress}%` }}>
+                </div>
             </div>
         </div>
     );
