9e7fb549c2328f6abb669ad9d1fcc48eceecd134
Mr-Ndi <onlythenotes@gmail.com>
2024-12-22 09:42:43 +0200

    finalised api call for axious instace

--- src/components/AuthContext/api.tsx
+++ src/components/AuthContext/api.tsx
@@ -1,6 +1,31 @@
 import axiosInstance from "./axiosInstance";
 
-const login = async (credentials) =>{
+interface LoginCredentials{
+    username: string;
+    password: string;
+}
+interface AccountData{
+    names: string;
+    email: string;
+    district: string;
+    sector: string;
+    telephone: string;
+    username: string;
+    profile_picture: string;
+}
+interface ProductDetails{
+    name: string;
+    price: number;
+    stock_quantity: number;
+    unit: string;
+    minimum_for_deliver: number;
+    description: string;
+    owner_id: number;
+}
+interface SingleProductParams{
+    name: string;
+}
+const login = async (credentials: LoginCredentials) =>{
     try{
         const response = await axiosInstance.post('bea/login/', credentials)
         return response.data;
@@ -9,7 +34,7 @@ const login = async (credentials) =>{
     }
 }
 
-const createAccount = async(accountData) => {
+const createAccount = async(accountData: AccountData) => {
     try{
         const response = await axiosInstance.post('bea/create-seller/', accountData)
         return response.data;
@@ -18,7 +43,7 @@ const createAccount = async(accountData) => {
     }
 };
 
-const upload = async(productDetails) => {
+const upload = async(productDetails: ProductDetails) => {
     try{
         const response = await axiosInstance.post('poli/product/', productDetails)
         return response.data;
@@ -27,9 +52,12 @@ const upload = async(productDetails) => {
     }
 }
 
-const singleProduct = async(name) =>{
+const singleProduct = async(params: SingleProductParams):
+Promise<ProductDetails> =>{
     try{
-        const response = await axiosInstance.get('/poli/products', name)
+        const response = await axiosInstance.get('/poli/products',{
+            params
+        })
         return response.data
     }catch(error){
         throw error
