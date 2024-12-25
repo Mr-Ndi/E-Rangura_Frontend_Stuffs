e1be87192961e82dfc702211c02139d3e902ff24
Mr-Ndi <onlythenotes@gmail.com>
2024-12-25 09:04:07 +0200

    changing time out

--- src/components/AuthContext/axiosInstance.tsx
+++ src/components/AuthContext/axiosInstance.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
     // baseURL: 'http://127.0.0.1:8000/',
     baseURL: 'https://sokoni-6ocg.onrender.com',
-    timeout: 10000,
+    timeout: 20000,
     headers: {
         'Content-Type': 'application/json',
     },
